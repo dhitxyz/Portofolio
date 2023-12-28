c97c7d58ad9314ae038f25ec9b7b7a395f8c8e3a
KadekPanjiii <dekpanji23@gmail.com>
2023-12-28 21:34:25 +0800

    Pliss

--- app/projects/dataProjects.js
+++ app/projects/dataProjects.js
@@ -9,19 +9,19 @@ import Image from "next/image";
 const Projects = [
   {
     id: 1,
-    judul: "Siskampus",
+    judul: "Sistem Kampus",
     tanggal: "15 Agustus 2023",
     foto: Foto1,
-    bahasa: ["HTML", "CSS", "PHP", "OOP", "SQL"],
-    tools: ["Boostrap", "PhpMyAdmin"],
+    bahasa: ["HTML", "CSS", "PHP", "JavaScript", "Boostrap"],
+    tools: ["GIT", "GitHub", "MySQL"],
     deskripsi: (
       <>
-        Siskampus merupakan website inovatif yang dirancang dengan paradigma
-        pemrograman berorientasi objek (OOP) dalam bahasa pemrograman PHP.
-        Dengan fokus pada kemudahan penggunaan dan pengelolaan data yang
-        efisien, Siskampus menyediakan berbagai fitur, antara lain sistem login
-        yang aman, pendaftaran mahasiswa, pengumuman, dan pengolahan data
-        mahasiswa, jurusan, dan mata pelajaran.
+        sebuah website inovatif yang dirancang dengan paradigma pemrograman
+        berorientasi objek (OOP) menggunakan bahasa pemrograman PHP. Dengan
+        fokus pada kemudahan penggunaan dan pengelolaan data yang efisien,
+        Siskampus menyajikan berbagai fitur, termasuk sistem login yang aman,
+        pendaftaran mahasiswa, pengumuman, serta pengolahan data mahasiswa,
+        jurusan, dan mata pelajaran.
       </>
     ),
   },
@@ -31,48 +31,49 @@ const Projects = [
     judul: "Akademik",
     tanggal: "15 Desember 2022",
     foto: Foto2,
-    bahasa: ["Java", "GUI", "SQL"],
-    tools: ["Netbeans", "PhpMyAdmin"],
+    bahasa: ["Java"],
+    tools: ["MySQL"],
     deskripsi: (
       <>
-        Akademik merupakan sebuah aplikasi yang dibuat dengan menggunakan bahasa
-        pemrograman Java. Aplikasi ini menawarkan serangkaian fitur penting,
-        antara lain sistem login, pengolahan data mahasiswa, data dosen, data
-        mata kuliah, data kelas, dan pengelolaan kehadiran. Dengan desain
-        sederhana dan fokus pada kemudahan penggunaan, Akademik memberikan
-        solusi terintegrasi untuk institusi pendidikan tinggi.
+        aplikasi yang dikembangkan dengan menggunakan bahasa pemrograman Java.
+        Aplikasi ini menyediakan serangkaian fitur penting, seperti sistem
+        login, pengolahan data mahasiswa, data dosen, data mata kuliah, data
+        kelas, dan manajemen kehadiran. Dengan desain yang sederhana dan fokus
+        pada kemudahan penggunaan, Akademik memberikan solusi terintegrasi untuk
+        institusi pendidikan tinggi.
       </>
     ),
   },
   {
     id: 3,
-    judul: "Laundry",
+    judul: "Kasir Laundry",
     tanggal: "17 September 2023",
     foto: Foto3,
-    bahasa: ["HTML", "CSS", "PHP", "SQL"],
-    tools: ["Laravel 10", "Bootstrap", "PhpMyAdmin"],
+    bahasa: ["HTML", "CSS", "PHP", "Laravel 10", "Bootstrap"],
+    tools: ["GIT", "GitHub", "MySQL"],
     deskripsi: (
       <>
-        Aplikasi laundry yang dibangun menggunakan framework Laravel,
-        menghadirkan solusi terintegrasi untuk pengelolaan laundry modern.
-        Dengan fokus pada kemudahan penggunaan dan efisiensi operasional,
-        aplikasi ini dilengkapi dengan berbagai fitur, antara lain sistem login
-        yang aman, pemrosesan data master, dan data transaksi.
+        dikembangkan menggunakan framework Laravel memberikan solusi
+        terintegrasi untuk pengelolaan laundry modern. Dengan fokus pada
+        kemudahan penggunaan dan efisiensi operasional, aplikasi ini dilengkapi
+        dengan berbagai fitur, termasuk sistem login yang aman, pemrosesan data
+        master, dan data transaksi.
       </>
     ),
   },
   {
     id: 4,
-    judul: "Personal Website",
+    judul: "Website Pribadi",
     tanggal: "14 Desember 2023",
     foto: Foto4,
-    bahasa: ["HTML", "CSS", "JavaScript"],
-    tools: ["TailwindCSS", "NextJS"],
+    bahasa: ["NextJS 13", "TailwindCSS"],
+    tools: ["GIT", "GitHub", "NPM"],
     deskripsi: (
       <>
-        Situs pribadi saya, saya membuat situs web ini untuk menampilkan profil,
-        keterampilan, dan proyek saya. Sekaligus tempat saya mencoba teknologi
-        baru.
+        diciptakan sebagai wadah untuk menampilkan profil, keterampilan, dan
+        proyek-proyek saya. Saya merancang situs web ini tidak hanya sebagai
+        representasi diri, tetapi juga sebagai tempat eksperimen untuk mencoba
+        teknologi-teknologi baru.
       </>
     ),
   },
@@ -85,9 +86,9 @@ const Projects = [
     tools: [],
     deskripsi: (
       <>
-        Situs web menakjubkan yang dirancang menggunakan HTML, CSS, dan
-        JavaScript, memanfaatkan teknologi ParallaxJS untuk memberikan efek
-        visual yang imersif dan dinamis. HTML CSS JavaScript
+        dibuat dengan HTML, CSS, dan JavaScript, dengan sentuhan tambahan dari
+        teknologi ParallaxJS untuk memberikan efek visual yang menarik dan
+        dinamis.
       </>
     ),
   },
